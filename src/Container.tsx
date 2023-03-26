2f7a49fdbef4671b5ceb5220d0b559295dd1200c
Tomoya Matsubara <toto.mamama@keio.jp>
2023-03-26 16:55:12 +0900

    feat: implement simple line chart

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,11 +1,13 @@
 import WebSocketStomp from "./HandleWebsocket";
 import style from "./css/Container.module.css";
+import LineChart from "./LineChart";
 
 const Container = () => {
     return (
         <div id={style.container}>
             <h1>Deadlift Form Checker</h1>
             <WebSocketStomp />
+            <LineChart />
         </div>
     );
 };
