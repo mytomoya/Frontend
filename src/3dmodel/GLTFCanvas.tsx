8534bc662712c23a9182cd8b64c8f3eb47dde2f4
Tomoya Matsubara <toto.mamama@keio.jp>
2023-03-31 22:36:18 +0900

    feat: fix style
    `<canvas>` no longer overflowed

--- src/3dmodel/GLTFCanvas.tsx
+++ src/3dmodel/GLTFCanvas.tsx
@@ -3,29 +3,33 @@ import { OrbitControls } from "@react-three/drei";
 import Model from "./Model";
 import { Suspense, useState } from "react";
 
+import style from "../css/GLTFCanvas.module.css";
+
 const GLTFCanvas = (): JSX.Element => {
     const [animate, setAniamte] = useState<boolean>(true);
 
     return (
-        <div style={{ height: "500px" }}>
+        <div>
             <h2>Animation</h2>
             <button onClick={() => setAniamte(!animate)}>
                 {animate ? "stop" : "start"}
             </button>
-            <Canvas
-                camera={{
-                    fov: 50,
-                    position: [0, 0, 3],
-                }}
-                style={{ border: "1px solid black" }}
-            >
-                <OrbitControls />
-                <ambientLight />
-                {/* <pointLight position={[10, 10, 10]} /> */}
-                <Suspense fallback={null}>
-                    <Model animate={animate} />
-                </Suspense>
-            </Canvas>
+            <div id={style.container}>
+                <Canvas
+                    camera={{
+                        fov: 50,
+                        position: [0, 0, 3],
+                    }}
+                    style={{ border: "1px solid black" }}
+                >
+                    <OrbitControls />
+                    <ambientLight />
+                    {/* <pointLight position={[10, 10, 10]} /> */}
+                    <Suspense fallback={null}>
+                        <Model animate={animate} />
+                    </Suspense>
+                </Canvas>
+            </div>
         </div>
     );
 };
