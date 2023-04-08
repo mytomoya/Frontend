06b55dbb1e875f10770c4f54900653e4ca85170e
Tomoya Matsubara <toto.mamama@keio.jp>
2023-04-08 23:16:09 +0900

    refactor: move table to `Table.tsx`

--- src/history/History.tsx
+++ src/history/History.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getRecords } from "../api/Handler";
 import { Item } from "../api/Types";
 import style from "../scss/History.module.scss";
-import { formatDate } from "../Helper";
+import Table from "./Table";
 
 const History = () => {
     const [checkedRecord, setCheckedRecord] = useState<number>(-1);
@@ -22,61 +22,13 @@ const History = () => {
         fetchData();
     }, []);
 
-    const list = (): JSX.Element => {
-        return (
-            <div id={style["table"]}>
-                <div id={style["header"]} className={style["item"]}>
-                    <div className={style["index"]}>Index</div>
-                    <div className={style["record"]}>Record</div>
-                </div>
-
-                {records.map((value, index) => {
-                    const date = Date.parse(value.datetime);
-                    const formattedDate = formatDate(date);
-                    const [day, time] = formattedDate.split(" ");
-
-                    let className = `${style["item"]}`;
-                    if (value.id === checkedRecord) {
-                        className += ` ${style["checked"]}`;
-                    }
-
-                    return (
-                        <div
-                            key={value.id}
-                            className={className}
-                            onClick={() => setCheckedRecord(value.id)}
-                        >
-                            <div className={style["index"]}>{index + 1}</div>
-                            <div className={style["record"]}>
-                                <span>{day}</span>
-                                <span> {time}</span>
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    };
-
-    let className = `${style["buttons"]}`;
-    if (checkedRecord === -1) {
-        className += ` ${style["disabled"]}`;
-    }
-
     return (
-        <div id={style["root"]}>
-            <h2>History</h2>
-            {list()}
-            <div className={className}>
-                <div>
-                    <button className="default-button">Show</button>
-                </div>
-                <div>
-                    <button className={`default-button ${style["delete"]}`}>
-                        Delete
-                    </button>
-                </div>
-            </div>
+        <div>
+            <Table
+                records={records}
+                checkedRecord={checkedRecord}
+                setCheckedRecord={setCheckedRecord}
+            />
         </div>
     );
 };
