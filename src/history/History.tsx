59ec9766e4281f216bd0b36dd828130ee78ec544
Tomoya Matsubara <toto.mamama@keio.jp>
2023-04-08 20:25:32 +0900

    feat: fetch data with `getRecords`

--- src/history/History.tsx
+++ src/history/History.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import { getRecords } from "../api/Handler";
+import { Item } from "../api/Types";
 import style from "../scss/History.module.scss";
+import { formatDate } from "../Helper";
 
 const History = () => {
-    const records = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    const [records, setRecords] = useState<Item[]>([]);
+
+    const fetchData = async (offset: number = 0) => {
+        const response = await getRecords(offset);
+
+        if (response === null) {
+            return;
+        }
+
+        setRecords(response.data);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     const list = (): JSX.Element => {
         return (
@@ -12,10 +30,15 @@ const History = () => {
                 </div>
 
                 {records.map((value, index) => {
+                    const date = Date.parse(value.datetime);
+                    const formattedDate = formatDate(date);
+
                     return (
-                        <div key={index} className={style["item"]}>
+                        <div key={value.id} className={style["item"]}>
                             <div className={style["index"]}>{index + 1}</div>
-                            <div className={style["record"]}>{value}</div>
+                            <div className={style["record"]}>
+                                {formattedDate}
+                            </div>
                         </div>
                     );
                 })}
